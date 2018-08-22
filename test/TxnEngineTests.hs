929fc24225bc92ae015b65107b2529faee0cde10
Iulian Dogariu <code@iuliandogariu.com>
2018-08-22 15:22:27 +0300

    working copy changes

--- test/TxnEngineTests.hs
+++ test/TxnEngineTests.hs
@@ -1,12 +1,11 @@
 import Test.HUnit
-import Test.Framework
-import Test.Framework.Providers.HUnit
-import Data.Monoid
+--import Test.Framework
+--import Test.Framework.Providers.HUnit
+--import Data.Monoid
 import Control.Monad
-import Utils
 
-pushTest :: Assertion
-pushTest = [NumLit 1] ^? push (NumLit 1)
+simpleTxnBalance :: Assertion
+simpleTxnBalance = [NumLit 1] ^? push (NumLit 1)
 
 pushPopTest :: Assertion
 pushPopTest = [] ^? (push (NumLit 0) >> void pop)
